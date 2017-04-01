2972233f3172fd81a85edf624b41350c6f40aace
sish <sishuai0328@gmail.com>
2017-04-02 00:25:02 +0800

    join & quit group action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -40,6 +40,32 @@ class GroupsController < ApplicationController
     redirect_to groups_path, alert: "Group deleted"
   end
 
+  def join
+    @group = Group.find(params[:id])
+
+    if !current_user.is_member_of?(@group)
+        current_user.join!(@group)
+        flash[:notice] = "加入本讨论组成功！"
+    else
+        flash[:warning] = "你已经是本讨论组成员了！"
+    end
+
+    redirect_to groups_path(@group)
+  end
+
+  def quit
+    @group = Group.find(params[:id])
+
+    if current_user.is_member_of?(@group)
+        current_user.quit!(@group)
+        flash[:alert] = "已退出本讨论组！"
+    else
+        flash[:warning] = "你不是本讨论组成员，无法进行退出操作！"
+    end
+
+    redirect_to groups_path(@group)
+  end
+
   private
 
   def find_group_and_check_permission
